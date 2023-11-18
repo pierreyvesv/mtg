9d7d47785d59489d59806411e48d380af1ca0d87
Pierre-Yves Verdon <euryel@yahoo.fr>
2023-11-18 14:11:27 +0100

    first push

---
+++ Gemfile
@@ -0,0 +1,17 @@
+#source "https://rubygems.org"
+#
+#gem "jekyll"
+#
+#group :jekyll_plugins do
+#  gem "jekyll-feed"
+#  gem "jekyll-seo-tag"
+#  gem "webrick"
+#end
+
+source "https://rubygems.org"
+
+gem "jekyll", "~> 3.7"
+gem "github-pages", group: :jekyll_plugins
+gem "jekyll-include-cache", group: :jekyll_plugins
+gem "kramdown", ">= 2.3.1"
+
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Tips to size Azure VM",
+        "excerpt":"VM Sizing guide This vm sizing guide is based on my experience. It’s a prerequisite before using the Azure-Calculator. This guide give hints on how to choose the best vm size for your workload. This is mostly a list of concept and document that you should use. Once you know...","categories": ["finops"],
+        "tags": [],
+        "url": "/mtg/finops/vm-sizing/",
+        "teaser": null
+      }]
