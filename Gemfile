ce40052234b40998a9c67608e5f755326b7bf5e0
Pierre-Yves Verdon <euryel@yahoo.fr>
2024-05-30 00:14:12 +0200

    test plugins

--- Gemfile
+++ Gemfile
@@ -2,16 +2,25 @@
 #
 #gem "jekyll"
 #
-#group :jekyll_plugins do
-#  gem "jekyll-feed"
-#  gem "jekyll-seo-tag"
-#  gem "webrick"
-#end
+group :jekyll_plugins do
+ gem "jekyll-feed"
+ gem "jekyll-seo-tag"
+ gem "webrick"
+ gem "jekyll-redirect-from"
+ gem "jekyll-paginate"
+ gem "jekyll-sitemap"
+ gem "jekyll-default-layout"
+ gem "kramdown-parser-gfm"
+ gem "kramdown-syntax-coderay"
+ gem "faraday-retry"
+end
 
 source "https://rubygems.org"
 
-gem "jekyll", "~> 3.7"
-gem "github-pages", group: :jekyll_plugins
+#gem "jekyll", "~> 3.7"
+#gem "github-pages", group: :jekyll_plugins
+gem 'jekyll', "~> 3.7", group: :jekyll_plugins
+gem "jekyll-gist"
 gem "jekyll-include-cache", group: :jekyll_plugins
 gem "kramdown", ">= 2.3.1"
 
---
+++ _plugins/mtg_card.rb
@@ -0,0 +1,54 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+module Jekyll
+  class MTGCardTag < Liquid::Tag
+    def initialize(tag_name, card_name, tokens)
+      super
+      @card_name = card_name.strip
+    end
+
+    def render(context)
+      card_data = fetch_card_data(@card_name)
+      
+      if card_data.nil?
+        return "Card not found: #{@card_name}"
+      end
+
+      card_url = card_data["scryfall_uri"]
+      image_url = card_data["image_uris"]["normal"]
+
+      html_output = <<~HTML
+      <div class="mtg-card">
+        <a href="#{card_url}" target="_blank">
+          <img src="#{image_url}" alt="#{escape_html(@card_name)}" />
+          
+          #{escape_html(@card_name)}
+        </a>
+      </div>
+      HTML
+
+      html_output.strip
+    end
+
+    private
+
+    def fetch_card_data(card_name)
+      uri = URI.parse("https://api.scryfall.com/cards/named?fuzzy=#{CGI.escape(card_name)}")
+      response = Net::HTTP.get_response(uri)
+
+      if response.is_a?(Net::HTTPSuccess)
+        JSON.parse(response.body)
+      else
+        nil
+      end
+    end
+
+    def escape_html(text)
+      CGI.escapeHTML(text)
+    end
+  end
+end
+
+Liquid::Template.register_tag('mtg_card', Jekyll::MTGCardTag)
